0e0e411c8e9d3656a84d3fa43da376f6fc11fea2
Peter Vila <peterdvila@gmail.com>
2021-09-20 17:31:10 -0700

    Added progress circles and 10 second loading bar animation to quiz page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var $navH1 = document.querySelector('.navh1');
+var $body = document.querySelector('body');
+
 var $startQuiz = document.querySelector('#start');
 $startQuiz.addEventListener('click', startQuizModal);
 var $startModal = document.querySelector('.modal-background')
@@ -10,4 +13,15 @@ var $submitButton = document.querySelector('#submit');
 $cancelButton.addEventListener('click', hideQuizModal);
 function hideQuizModal(){
     $startModal.className = "modal-background hidden"
+}
+$submitButton.addEventListener('click', startQuiz);
+
+var $homePage = document.querySelector('div[data-view="home"]')
+var $quizPage = document.querySelector('div[data-view="quiz"]')
+function startQuiz(){
+    $startModal.className = "modal-background hidden"
+    $homePage.className = "container hidden"
+    $quizPage.className = "container"
+    $body.className = "blueBackground"
+    $navH1.textContent = "Question " + dataModel.question
 }
